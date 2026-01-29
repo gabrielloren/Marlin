c6e0c04eba8ac1a82bee85207bd828f0d3cc276a
gabrielloren <gabriel_loren@hotmail.com>
2026-01-28 23:33:04 -0300

    Corrected FAN pins and AUTO_FAN

--- Marlin/src/pins/stm32g0/pins_BTT_SKR_MINI_E3_V3_0.h
+++ Marlin/src/pins/stm32g0/pins_BTT_SKR_MINI_E3_V3_0.h
@@ -149,7 +149,7 @@
 #define HEATER_BED_PIN                      PC9   // "HB"
 #define FAN0_PIN                            PC6   // "FAN0"
 #define FAN1_PIN                            PC7   // "FAN1"
-#define FAN2_PIN                            PB15  // "FAN2"
+//#define FAN2_PIN                            PB15  // "FAN2"
 
 /**
  *              SKR Mini E3 V3.0
